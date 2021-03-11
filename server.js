498cf4772d4ac90c94de01b3431df9397329f2e1
danteYoon <dante@watcha.com>
2021-03-11 22:08:55 +0900

    UpdatUpdate: simple server built

---
+++ server.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const http = require("http");
+
+let html;
+let js;
+let css;
+
+fs.readFile("./index.html", (err,data) => {
+  if(err){
+    throw err;
+  }
+  html = data;
+})
+fs.readFile("./index.js", (err,data) => {
+  if(err){
+    throw err;
+  }
+  js = data;
+})
+fs.readFile("./style.css", (err,data) => {
+  if(err) {
+    throw err;
+  }
+  css =data;
+})
+
+http.createServer((req,res) =>{
+  res.statusCode = 200;
+  
+  if(req.url.indexOf('.css') != -1){
+    res.writeHead(200, {'Content-Type': 'text/css'});
+    res.write(css);
+    res.end();
+    return;
+   }
+   if(req.url.indexOf('index.js') != -1){
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    res.write(js);
+    res.end();
+    return;
+   }
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write(html);
+  res.end();
+  
+}).listen(3000);
+
+console.log("server is running on port 3000");
